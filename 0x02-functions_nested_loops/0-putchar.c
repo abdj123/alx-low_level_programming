30aee8e22e843110f392f0126e409d8cf325a61b
z-qine <zqine@localhost.localdomain>
2023-07-18 10:53:26 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s","_putchar");
+
+    printf("\n");
+
+	return (0);
+}
