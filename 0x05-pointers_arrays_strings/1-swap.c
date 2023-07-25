e9aaa7e4d090abcb8cea880f4f3f1f95fc8131e7
z-qine <zqine@localhost.localdomain>
2023-07-25 11:55:37 +0300

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * 1-swap -
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	tmp=*a;
+	*a=*b;
+	*b=tmp;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * 4-print_rev
+ */
+void print_rev(char *s)
+{
+	int i=0;
+	while(str[i]!="\0")
+	{
+	    i++;
+	}
+	while(str[i]!="\0")
+	{
+	    _putchar(str[i]);
+	    i--;
+	}
+	_putchar("\n");
+	
+}
