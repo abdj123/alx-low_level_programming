09f70d709fd4e20d625c744ad99266227230e95e
z-qine <zqine@localhost.localdomain>
2023-08-01 10:56:19 +0300

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - Fills memory with a constant byte
+ *
+ * @s: pointer
+ * @b: char
+ * @n: unsigned int
+ *
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
